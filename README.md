418362c2a070f092b9bc01c534cd2f182eaa13b6
Manjunath Jidagi <manjujidagi@protonmail.com>
2022-01-23 17:27:47 +0530

    Fixed Readme

--- README.md
+++ README.md
@@ -21,38 +21,16 @@ ___
 ```jsx
 import React from 'react'
 
-import { ReactWebStore } from 'react-webstore';
+import { HelloComponent } from './Components/HelloComponent';
+import { WorldComponent } from './Components/WorldComponent';
 
 const App = () => {
 
-  const rws = new ReactWebStore();
-
-  rws.subscribe('key1', (e) => {
-      let payload = rws.getStore(e.type);
-      console.log("Payload @ key1 - ", payload)
-  })
-
-  rws.subscribe('key2', (e) => {
-      let payload = rws.getStore(e.type);
-      console.log("Payload @ key2 - ", payload)
-  })
-
   return (
     <div>
-      
-      <button onClick={() => {
-          rws.store('key1', 'Hello')
-      }}>Click To Store @ key1</button>
 
-      <button onClick={() => {
-          rws.store('key2', 'World')
-      }}>Click To Store @ key2</button>
-
-      <br /><br />
-
-      <button onClick={() => {
-          console.log(rws.getStore())
-      }}>Click To Show</button>
+      <HelloComponent />
+      <WorldComponent />
 
     </div>
   )
--- example/src/App.js
+++ example/src/App.js
@@ -1,43 +1,15 @@
 import React from 'react'
 
-import { ReactWebStore } from 'react-webstore';
 import { HelloComponent } from './Components/HelloComponent';
 import { WorldComponent } from './Components/WorldComponent';
 
 const App = () => {
-
-  const rws = new ReactWebStore();
-  const rws2 = new ReactWebStore();
-
-  rws.subscribe('key1', (e) => {
-      let payload = rws.getStore(e.type);
-      console.log("Payload @ key1 - ", payload)
-  })
-
-  rws2.subscribe('key2', (e) => {
-      let payload = rws.getStore(e.type);
-      console.log("Payload @ key2 - ", payload)
-  })
 
   return (
     <div>
 
       <HelloComponent />
       <WorldComponent />
-      
-      {/* <button onClick={() => {
-          rws.store('key1', 'Hello')
-      }}>Click To Store @ key1</button>
-
-      <button onClick={() => {
-          rws.store('key2', 'World')
-      }}>Click To Store @ key2</button>
-
-      <br /><br />
-
-      <button onClick={() => {
-          console.log(rws.getStore())
-      }}>Click To Show</button> */}
 
     </div>
   )
